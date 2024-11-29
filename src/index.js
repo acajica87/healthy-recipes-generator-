047a113d49219c2658440ec67ee8792c3148e20e
acajica87 <acajica85@gmail.com>
2024-11-28 19:41:29 -0600

    add js and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Spiced carrot & lentil soup",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#recipe-generator-form");
+poemFormElement.addEventListener("submit", generateRecipe);
